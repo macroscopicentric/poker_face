6f028305b3f3d60e75e98fac197b97ab6cb686a4
Rachel King <rachel.b.king@gmail.com>
2014-10-07 11:33:01 -0700

    Updated sorting and sorting tests.

--- poker.py
+++ poker.py
@@ -1,17 +1,27 @@
 def sort_hand(hand):
     """Given a hand (list of strings), will sort in order from highest to lowest cards and return as a list of strings."""
     face_cards_score = {'A': 14, 'K': 13, 'Q': 12, 'J': 11}
+    reverse_face_cards = {14: 'A', 13: 'K', 12: 'Q', 11: 'J'}
     sorted_hand = []
 
     #convert face cards to numbers, turn each string into a tuple of (number, suite), sort, then turn back into a sorted list of strings.
+    #this doesn't currently address sorting the suites.
     for card in hand:
         if card[0] in face_cards_score:
+            #if it's a face card
             new_card = (face_cards_score[card[0]], card[1])
+        elif len(card) == 3:
+            #if the card is a 10, it's a different string length
+            new_card = (int(card[0:2]), card[2])
         else:
-            new_card = (card[0], card[1])
+            new_card = (int(card[0]), card[1])
         sorted_hand.append(new_card)
 
     sorted_hand.sort()
+    #sorting from highest to lowest in the assumption that the best scoring hand will include the highest cards possible.
+    sorted_hand.reverse()
+
+    sorted_hand = [str(reverse_face_cards[card[0]]) + card[1] if card[0] in reverse_face_cards else str(card[0]) + card[1] for card in sorted_hand]
 
     return sorted_hand
 
--- tests/test_poker.py
+++ tests/test_poker.py
@@ -4,9 +4,19 @@ from nose.tools import eq_
 from poker_face.poker import sort_hand, find_highest_hand
 
 class TestSortHand(unittest.TestCase):
-    def test_simple(self):
+    def test_same_suite(self):
         result = sort_hand(['10S', 'JS', 'QS', 'KS', 'AS'])
         expected_result = ['AS', 'KS', 'QS', 'JS', '10S']
+        eq_(result, expected_result)
+
+    def test_all_suites(self):
+        result = sort_hand(['10S', '10H', '10C', '10D'])
+        expected_result = ['10S', '10H', '10D', '10C']
+        eq_(result, expected_result)
+
+    def test_mix(self):
+        result = sort_hand([['KS', 'KD', '3C', '3H', '3D']])
+        expected_result = ['KS', 'KD', '3H', '3D', '3C']
         eq_(result, expected_result)
 
 class TestFindHighestHand(unittest.TestCase):
@@ -22,12 +32,12 @@ class TestFindHighestHand(unittest.TestCase):
 
     def test_four_of_a_kind(self):
         result = find_highest_hand(['QS', 'QD', 'QC', 'QH', '5S'])
-        expected_result = ('Four of a Kind', ['QS', 'QC', 'QD', 'QH'])
+        expected_result = ('Four of a Kind', ['QS', 'QH', 'QD', 'QC'])
         eq_(result, expected_result)
 
     def test_full_house(self):
         result = find_highest_hand(['KS', 'KD', '3C', '3H', '3D'])
-        expected_result = ('Full House', ['KS', 'KD', '3C', '3D', '3H'])
+        expected_result = ('Full House', ['KS', 'KD', '3H', '3D', '3C'])
         eq_(result, expected_result)
 
     def test_flush(self):
@@ -42,12 +52,12 @@ class TestFindHighestHand(unittest.TestCase):
 
     def test_three_of_a_kind(self):
         result = find_highest_hand(['5D', 'JC', '8H', '8C', '8D'])
-        expected_result = ('Three of a Kind', ['8C', '8D', '8H'])
+        expected_result = ('Three of a Kind', ['8H', '8D', '8C'])
         eq_(result, expected_result)
 
     def test_two_pair(self):
         result = find_highest_hand(['10S', 'QD', '7S', 'QC', '7H'])
-        expected_result = ('Two Pair', ['QC', 'QD', '7S', '7H'])
+        expected_result = ('Two Pair', ['QD', 'QC', '7S', '7H'])
         eq_(result, expected_result)
 
     def test_pair(self):
