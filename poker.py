97d5d55256308a6e6f4f333e0037827053cdfb04
Rachel King <rachel.b.king@gmail.com>
2014-10-12 23:05:44 -0700

    Changed sort_hand to use OrderedDict.

--- poker.py
+++ poker.py
@@ -1,33 +1,26 @@
+import collections
+
 def sort_hand(hand):
     """Given a hand (list of strings), will sort in order from highest to lowest cards and return as a list of strings."""
-    face_cards_score = {'A': 14, 'K': 13, 'Q': 12, 'J': 11}
-    reverse_face_cards = {14: 'A', 13: 'K', 12: 'Q', 11: 'J'}
-    sorted_hand = []
+    #OrderedDict should be an easier way to accomplish the sorting.
+    card_count = collections.OrderedDict()
+    for face in ['A', 'K', 'Q', 'J']:
+        card_count[face] = []
+    for num in range(10, 1, -1):
+        card_count[num] = []
 
     #convert face cards to numbers, turn each string into a tuple of (number, suite), sort, then turn back into a sorted list of strings.
-    #this doesn't currently address sorting the suites.
+    #OrderedDict suites aren't sorted. Do I care?
     for card in hand:
-        if card[0] in face_cards_score:
-            #if it's a face card
-            new_card = (face_cards_score[card[0]], card[1])
-        elif len(card) == 3:
-            #if the card is a 10, it's a different string length
-            new_card = (int(card[0:2]), card[2])
-        else:
-            new_card = (int(card[0]), card[1])
-        sorted_hand.append(new_card)
+        suite = card[-1]
+        score = card[0:2] if len(card) == 3 else card[0]
+        card_count[score].append(suite)
 
-    sorted_hand.sort()
-    #sorting from highest to lowest in the assumption that the best scoring hand will include the highest cards possible.
-    sorted_hand.reverse()
-
-    sorted_hand = [str(reverse_face_cards[card[0]]) + card[1] if card[0] in reverse_face_cards else str(card[0]) + card[1] for card in sorted_hand]
-
-    return sorted_hand
+    return card_count
 
 def find_highest_hand(hand):
     """Given a hand (list of strings), will return the name of the highest ranking hand and a list of strings of the cards that fulfill it."""
-    sorted_hand = sort_hand(hand)
+    card_count = sort_hand(hand)
 
     pass
 
